b7c4f262509edd78dfb3dc01d0aacc35ed465d90
Yuriy <yurivalkiv@gmail.com>
2016-08-25 16:03:31 +0300

    service for shopping cart

--- src/main/java/com/myshop/yura/service/ShoppingCartService.java
+++ src/main/java/com/myshop/yura/service/ShoppingCartService.java
@@ -1,7 +1,40 @@
 package com.myshop.yura.service;
 
-/**
- * Created by valkiv on 8/25/16.
- */
+import com.myshop.yura.DAO.ShoppingCartDAO;
+import com.myshop.yura.entity.ShoppingCart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartDAO shoppingCartDAO;
+
+    @Transactional
+    public ShoppingCart getCart(long id) {
+        return shoppingCartDAO.getCart(id);
+    }
+
+    @Transactional
+    public void removeCart(long id) {
+        shoppingCartDAO.removeCart(id);
+    }
+
+    @Transactional void editCart(ShoppingCart shoppingCart) {
+        shoppingCartDAO.editCart(shoppingCart);
+    }
+
+    @Transactional
+    public void addCart(ShoppingCart shoppingCart) {
+        shoppingCartDAO.addCart(shoppingCart);
+    }
+
+    @Transactional
+    public List<ShoppingCart> getByUser(String user) {
+        return shoppingCartDAO.getByUser(user);
+    }
 }
